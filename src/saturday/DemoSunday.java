4a9f96b4752c5cc0541a614076ffb8ea154fdb61
paolarome <91574365+paolarome@users.noreply.github.com>
2021-10-02 11:17:30 -0400

    Update DemoSunday.java

--- src/saturday/DemoSunday.java
+++ src/saturday/DemoSunday.java
@@ -4,7 +4,7 @@ public class DemoSunday {
 	public static void main(String[] args) {
 		System.out.println("Saturday Demo");
 		System.out.println("xyz");
-		//Paola
+		//Paola from Github
 		int a = 10;
 		int b= 20;
 	}
